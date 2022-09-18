cecb16a0ebb5700908bf5af0dc54d696b0c69856
Александр <alez.grante@gmail.com>
2022-09-18 15:46:20 +0300

    Situation with side effect, Snack bar shows after configuration change

--- app/src/main/java/com/example/customlivedata/MainActivity.kt
+++ app/src/main/java/com/example/customlivedata/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.customlivedata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +21,14 @@ class MainActivity : AppCompatActivity() {
         viewModel =  ViewModelProvider(this)[MainViewModel::class.java]
         setContentView(R.layout.activity_main)
 
-        R.
+        findViewById<Button>(R.id.button).setOnClickListener {
+            viewModel.trigger()
+        }
+
+        viewModel.event.observe(this){
+            if(it)
+                Snackbar.make(findViewById(R.id.activity_layout),"Hello there",Snackbar.LENGTH_LONG).show()
+        }
 
     }
 }
